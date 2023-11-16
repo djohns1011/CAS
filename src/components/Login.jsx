7b504e2f03957b1b552f6fd1e8871fa1544ef140
Dennis Johns <99817065+djohns1011@users.noreply.github.com>
2023-11-16 23:29:40 +0530

    login page updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,93 @@
 import React from "react";
-import user from "../img/user.png";
+import { useRef, useState, useEffect, useContext } from "react";
+import userLogo from "../img/userLogo.png";
 import "../Login.css";
+import AuthContext from "../context/AuthProvider";
+import axios from "../api/axios";
+const LOGIN_URL = '/auth';
 
 function Login() {
+  const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(LOGIN_URL, JSON.stringify({ user, pwd }),
+      {
+        headers: { 'Content-Type': 'applications/json'},
+        withCredentials: true
+      }
+      );
+      console.log(JSON.stringify(response?.data))
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data.roles;
+      setAuth({ user, pwd, roles, accessToken });
+      setUser("");
+      setPwd("");
+    } catch (err){
+      if (!err?.response) {
+        setErrMsg('No Server Response');
+      } else if (err.response?.status === 400) {
+        setErrMsg('Missing Username or Password');
+      } else if (err.response?.status === 450) {
+        setErrMsg('Unauthorized');
+      } else {
+        setErrMsg('Login Failed');
+      }
+      errRef.current.focus();
+    }
+  };
+
   return (
-    
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to home</a>
+          </p>
+        </section>
+      ) : (
         <div className="login-card">
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}{" "}
+          </p>
           <div className="user-icon">
-            <img src={user} className="user-logo" alt="user-logo" />
+            <img src={userLogo} className="user-logo" alt="user-logo" />
           </div>
           <h3 className="login-title">LOGIN</h3>
-          <form className="login-form">
+          <form className="login-form" onSubmit={handleSubmit}>
             <div className="form-group">
               <input
                 type="text"
                 className="username"
+                ref={userRef}
+                autoComplete="off"
                 placeholder="Username"
-                required=""
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
               />
             </div>
             <div className="form-group">
@@ -24,7 +95,9 @@ function Login() {
                 type="password"
                 className="password"
                 placeholder="Password"
-                required=""
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
               />
             </div>
             <div className="form-group">
@@ -34,7 +107,8 @@ function Login() {
             </div>
           </form>
         </div>
-
+      )}
+    </>
   );
 }
 
