a69de16b40035582e5b430614627cdb1463919dd
Dennis Johns <99817065+djohns1011@users.noreply.github.com>
2023-12-03 13:08:49 +0530

    updated TeacherProfile

--- src/components/TeacherProfile.jsx
+++ src/components/TeacherProfile.jsx
@@ -1,61 +1,61 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function TeacherProfile() {
-    const [teacherTotal, setTeacherTotal] = useState(0)
-    const [studentTotal, setStudentTotal] = useState(0)
-  
-    useEffect(() => {
-      teacherCount();
-      studentCount();
-    }, [])
-  
-  
-    const teacherCount = () => {
-      axios.get('http://localhost:3001/auth/teacher_count')
-      .then(result => {
-        if(result.data.Status) {
-          setTeacherTotal(result.data.Result[0].teacher)
-        }
-      })
-    }
-    
-    const studentCount = () => {
-      axios.get('http://localhost:3001/auth/student_count')
-      .then(result => {
-        if(result.data.Status) {
-          setStudentTotal(result.data.Result[0].student)
-        } else {
-          alert(result.data.Error)
-        }
-      })
-    }
-    return (
-      <div>
-        <div className='p-3 d-flex justify-content-around mt-3'>
-          <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
-            <div className='text-center pb-1'>
-              <h4>Teachers</h4>
-            </div>
-            <hr />
-            <div className='d-flex justify-content-between'>
-              <h5>Total:</h5>
-              <h5>{teacherTotal}</h5>
-            </div>
+  const {teacherId} = useParams();
+  console.log(teacherId);
+  const [teacherTotal, setTeacherTotal] = useState(0);
+  const [studentTotal, setStudentTotal] = useState(0);
+
+  useEffect(() => {
+    teacherCount();
+    studentCount();
+  }, []);
+
+  const teacherCount = () => {
+    axios.get("http://localhost:3001/authT/teacher_count").then((result) => {
+      if (result.data.Status) {
+        setTeacherTotal(result.data.Result[0].teacher);
+      }
+    });
+  };
+
+  const studentCount = () => {
+    axios.get("http://localhost:3001/authT/student_count/" + teacherId).then((result) => {
+      if (result.data.Status) {
+        setStudentTotal(result.data.Result[0].student);
+      } else {
+        alert(result.data.Error);
+      }
+    });
+  };
+  return (
+    <div>
+      <div className="p-3 d-flex justify-content-around mt-3">
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Teachers</h4>
           </div>
-          <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
-            <div className='text-center pb-1'>
-              <h4>Students</h4>
-            </div>
-            <hr />
-            <div className='d-flex justify-content-between'>
-              <h5>Total:</h5>
-              <h5>{studentTotal}</h5>
-            </div>
+          <hr />
+          <div className="d-flex justify-content-between">
+            <h5>Total:</h5>
+            <h5>{teacherTotal}</h5>
+          </div>
+        </div>
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Students</h4>
+          </div>
+          <hr />
+          <div className="d-flex justify-content-between">
+            <h5>Total:</h5>
+            <h5>{studentTotal}</h5>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
 
-export default TeacherProfile
+export default TeacherProfile;
