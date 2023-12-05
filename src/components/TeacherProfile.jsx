fd303dbd865b96c0cb856ad7d3a1272fb473fb02
Dennis Johns <99817065+djohns1011@users.noreply.github.com>
2023-12-05 19:46:19 +0530

    updated TeacherProfile

--- src/components/TeacherProfile.jsx
+++ src/components/TeacherProfile.jsx
@@ -4,22 +4,21 @@ import { useParams } from "react-router-dom";
 
 function TeacherProfile() {
   const {teacherId} = useParams();
-  console.log(teacherId);
-  const [teacherTotal, setTeacherTotal] = useState(0);
   const [studentTotal, setStudentTotal] = useState(0);
+  const [teacherProfile, setTeacherProfile] = useState([]);
 
   useEffect(() => {
-    teacherCount();
     studentCount();
   }, []);
 
-  const teacherCount = () => {
-    axios.get("http://localhost:3001/authT/teacher_count").then((result) => {
-      if (result.data.Status) {
-        setTeacherTotal(result.data.Result[0].teacher);
-      }
-    });
-  };
+  useEffect(() => {
+    // Fetch teacher profile data from the backend
+    axios.get('http://localhost:3001/authT/teacher-profile/'+teacherId)
+      .then(result => {
+        setTeacherProfile(result.data[0])
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  });
 
   const studentCount = () => {
     axios.get("http://localhost:3001/authT/student_count/" + teacherId).then((result) => {
@@ -34,13 +33,17 @@ function TeacherProfile() {
     <div>
       <div className="p-3 d-flex justify-content-around mt-3">
         <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
-          <div className="text-center pb-1">
-            <h4>Teachers</h4>
+          <div className="pb-1">
+            <h5 className="mt-3">
+              <strong>Teacher Name:</strong> {teacherProfile.teacher_name}
+            </h5>
+            <h5></h5>
           </div>
           <hr />
           <div className="d-flex justify-content-between">
-            <h5>Total:</h5>
-            <h5>{teacherTotal}</h5>
+            <h5>
+              <strong>Class:</strong> {teacherProfile.class}
+            </h5>
           </div>
         </div>
         <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
